c3083395b93e73c8561ed0a3aec4b9f58e903bf3
Avigayil Sperka <avigayilmarcus@Avigayils-MacBook-Air-2.local>
2023-02-17 07:56:54 -0500

    Added for loop to display table of values.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿Console.WriteLine("N\tN^2\tN^3\tN^2 + N^3");
 
+for (var counter = 10; counter < 60; counter += 10)
+{
+    Console.WriteLine($"{counter}\t{Math.Pow(counter,2)}\t{Math.Pow(counter,3)}\t{Math.Pow(counter,2)+Math.Pow(counter,3)}");
+}
