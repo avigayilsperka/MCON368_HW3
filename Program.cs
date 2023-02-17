4ef8aaffa8bd8ecb7c43a0378b0ce5dd4ec2583d
Avigayil Sperka <avigayilmarcus@Avigayils-MacBook-Air-2.local>
2023-02-17 07:58:13 -0500

    Added Console.WriteLine message.

--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ for (var counter = 10; counter < 60; counter += 10)
 {
     Console.WriteLine($"{counter}\t{Math.Pow(counter,2)}\t{Math.Pow(counter,3)}\t{Math.Pow(counter,2)+Math.Pow(counter,3)}");
 }
+Console.WriteLine("Press any key to continue...");
